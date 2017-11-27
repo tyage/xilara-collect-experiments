4fb440d7d8ef449fc47e141e37d761e3da87fdd4
tyage <namatyage@gmail.com>
2017-11-27 23:43:26 +0900

    fetch partial reports

--- xss-db/collect-reports.mjs
+++ xss-db/collect-reports.mjs
@@ -1,18 +1,20 @@
 import fs from 'fs';
 import { fetch, sleep } from './lib';
 
-const fetchOpenBugBounty = (id) => fetch(`https://www.openbugbounty.org/reports/${id}/`);
-const getLatestOpenBugBountyId = () => {
-  // TODO: implement
-  return 432289;
-};
+const reports = JSON.parse(fs.readFileSync('data/openbugbounty/reports.json'));
+const unpatchedXSSReports = reports.filter(r => r.status === 'unpatched' && r.type === 'Cross Site Scripting');
 
-const fetchAllOpenBugBounties = async () => {
-  for (let id = getLatestOpenBugBountyId(); id > 0; --id) {
-    const filename = `data/openbugbounty/${id}`;
-    const body = await fetchOpenBugBounty(id);
+const fetchAllReports = async () => {
+  for (let report of unpatchedXSSReports) {
+    const { id } = report;
+    // XXX: use partial reports
+    if (+id % 10 !== 0) {
+      continue;
+    }
+    const filename = `data/openbugbounty/reports/${id}`;
+    const body = await fetch(`https://www.openbugbounty.org/reports/${id}/`);
     fs.writeFileSync(filename, body);
     await sleep(500);
   }
 };
-fetchAllOpenBugBounties();
+fetchAllReports();
