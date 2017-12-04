faf2035e4008fd674815abfc909a8d1c628798cd
tyage <namatyage@gmail.com>
2017-12-04 14:20:33 +0900

    use cheerio to parse html

--- xss-db/collect-responses.mjs
+++ xss-db/collect-responses.mjs
@@ -1,13 +1,20 @@
 import fs from 'fs';
-import { listFiles, parseHTML } from './lib';
+import cheerio from 'cheerio';
+import { listFiles } from './lib';
 
-const collectResponse = async (report) => {};
+const reportDir = 'data/openbugbounty/reports';
+const collectResponse = async (report) => {
+  const contents = fs.readFileSync(`${reportDir}/${report}`);
+  const $ = cheerio.load(contents.toString());
+  $('.url').each((i, e) => {
+    console.log($(e).html());
+  });
+};
 
 const collectResponses = async () => {
-  const dir = 'data/openbugbounty/reports';
-  const files = await listFiles(dir);
+  const files = await listFiles(reportDir);
   const reportFiles = files.filter(f => /^\d+$/.test(f));
-  reportFiles.forEach(report => {
+  reportFiles.forEach(async (report) => {
     await collectResponse(report);
   });
 };
