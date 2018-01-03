6a82cb752abfed4b4b20387da6b714a2dc546e67
tyage <namatyage@gmail.com>
2018-01-03 16:26:39 +0900

    handle javascript dialog

--- xss-db/scripts/collect-valid-reports.mjs
+++ xss-db/scripts/collect-valid-reports.mjs
@@ -20,7 +20,7 @@ const checkDialogOpening = async (Page, url) => {
   const timeout = 1000 * 5;
   return new Promise((resolve, reject) => {
     Page.javascriptDialogOpening(() => {
-      Page.javascriptDialogOpening(false);
+      Page.handleJavaScriptDialog({ accept: false });
       resolve(true);
     });
     Page.navigate({ url });
