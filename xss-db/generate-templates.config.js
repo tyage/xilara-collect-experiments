1f9e4a6b5a8de547d1fa36c0562e049733e6abfb
tyage <namatyage@gmail.com>
2018-01-08 19:12:00 +0900

    add pm2 to generate templates

---
+++ xss-db/generate-templates.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'generate-templates',
+      script    : './scripts/generate-templates.mjs',
+      node_args : '--experimental-modules'
+    }
+  ]
+};
