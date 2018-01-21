84a72114238503348884f3a3486ea1ba9dd41feb
tyage <namatyage@gmail.com>
2018-01-21 15:13:30 +0900

    check all files exists

--- xss-db/scripts/template-matching.mjs
+++ xss-db/scripts/template-matching.mjs
@@ -12,34 +12,40 @@ const analyzeTemplateMatchingResult = async () => {
     const responseDir = `${responsesDir}/${report}`;
     const roadrunnerFile = `data/openbugbounty/templates/${report}`;
 
-    if (!fs.existsSync(roadrunnerFile)) {
+    const safeResponses = [1, 2, 3].map(id => `${responseDir}/${id}`);
+    const pocResponses = ['poc'].map(id => `${responseDir}/${id}`);
+
+    let allFilesExist = true;
+    for (let file of [ roadrunnerFile, ...safeResponses, ...pocResponses ]) {
+      if (!fs.existsSync(file)) {
+        allFilesExist = false;
+        break;
+      }
+    }
+    if (!allFilesExist) {
       continue;
     }
 
     const htmls = [1, 2].map(id => `${responseDir}/${id}`);
-    const formattedHTMLs = await Promise.all(htmls.map(async (html) => {
-      return (fs.readFileSync(html).toString());
+    const formattedHTMLs = await Promise.all(htmls.map(async (htmlFile) => {
+      const html = fs.readFileSync(htmlFile).toString();
+      return await formatHTMLByChrome(html);
     }));
     const template = await generateTemplateFromRoadrunnerFile(roadrunnerFile, formattedHTMLs);
 
-    const safeResponses = [1, 2, 3].map(id => `${responseDir}/${id}`);
-    const pocResponses = ['poc'].map(id => `${responseDir}/${id}`);
-
+    console.log(`===== start report ${report}`);
     for (let html of safeResponses) {
       const htmlContent = fs.readFileSync(html);
       const { result, matchMap } = await isHTMLMatchWithTemplate(htmlContent, template);
       // result should be true
-      console.log(html, result, matchMap);
-      break;
+      console.log(html, result);
     }
     for (let html of pocResponses) {
       const htmlContent = fs.readFileSync(html);
       const { result } = await isHTMLMatchWithTemplate(htmlContent, template);
       // result should not be true
       console.log(html, result);
-      break;
     }
-    break;
   }
 };
 analyzeTemplateMatchingResult();
